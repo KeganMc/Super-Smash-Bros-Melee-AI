00e889bd8171913927ca31c4a0968683085115de
keganmc <kmcilwai@uwyo.edu>
2016-11-06 12:01:03 -0700

    The code will find the dolphin directory and make the fifo if it doesn't exist

--- Source/BigProject.py
+++ Source/BigProject.py
@@ -1,9 +1,34 @@
 import os, time
+def find_directory():
+  possible = ["~/.dolphin-emu", "~/.local/share/.dolphin-emu", "~/Library/Application/ Support/Dolphin", "~/.local/share/dolphin-emu"]
+  for path in possible:
+    fullPath = os.path.expanduser(path)
+    if os.path.isdir(fullPath):
+      return fullPath
+  return None
+
+def find_make_pipe_dir(dolphinPath):
+  pipesPath = dolphinPath + "/Pipes"
+  if os.path.isdir(pipesPath):
+    return pipesPath
+  os.mkdir(pipesPath)
+  return pipesPath
 
 def main():
   i = 0
+  dolphinPath = find_directory()
+  if dolphinPath is None:
+    print("Could not find dolphin directory!")
+    return
+
+  pipe = find_make_pipe_dir(dolphinPath) + "/pipe"
+  try:
+    os.mkfifo(pipe)
+  except OSError:
+    pass
+
+  pipeout = open(pipe, "w")
   while(True):
-    pipeName = "../../../../.local/share/dolphin-emu/Pipes/pipe"
     pipeout = open(pipeName, "w")
     pipeout.write("SET MAIN 0.5 0.3\n")
     pipeout.write("PRESS B\n")
