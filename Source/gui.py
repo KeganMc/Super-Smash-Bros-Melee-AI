f677d0e97001d67584bba81b08a74bbee40b517f
PulvarizerCoop1 <pulvarizercoop1@gmail.com>
2017-02-13 21:05:32 -0700

    GUI update
    Launch button should be working

---
+++ Source/gui.py
@@ -0,0 +1,21 @@
+import tkinter
+import BigProject
+
+window = tkinter.Tk()
+
+# Launch BigProject.py
+def BP():
+	BigProject
+
+# Set up title and icon
+window.title("Super Smash Bros AI")
+window.geometry("200x200")
+#window.wm_iconbitmap('SmashIcon.ico')
+
+# Create and Add widgets
+launch = tkinter.Button(window, text = "Launch Bot", command=BP)
+launch.pack()
+
+
+window.mainloop()
+
