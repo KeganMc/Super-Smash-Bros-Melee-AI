d135ea54585efc3cb76eefcb9fbb951b57fc5a01
Jay Bishop <jaycgtsp@gmail.com>
2017-02-15 10:36:20 -0700

    Updates gui to have Canvas

--- Source/gui.py
+++ Source/gui.py
@@ -1,7 +1,26 @@
-import tkinter
-import BigProject
+from tkinter import *
+#import BigProject
 
-window = tkinter.Tk()
+window = Tk()
+
+# a subclass of Canvas for dealing with resizing of windows
+class ResizingCanvas(Canvas):
+    def __init__(self,parent,**kwargs):
+        Canvas.__init__(self,parent,**kwargs)
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
+
+    def on_resize(self,event):
+        # determine the ratio of old width/height to new width/height
+        wscale = float(event.width)/self.width
+        hscale = float(event.height)/self.height
+        self.width = event.width
+        self.height = event.height
+        # resize the canvas 
+        self.config(width=self.width, height=self.height)
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all",0,0,wscale,hscale)
 
 # Launch BigProject.py
 def BP():
@@ -12,10 +31,14 @@ window.title("Super Smash Bros AI")
 window.geometry("200x200")
 window.wm_iconbitmap('SmashIcon.ico')
 
-# Create and Add widgets
-launch = tkinter.Button(window, text = "Launch Bot", command=BP)
-launch.pack()
+myframe = Frame(window)
+myframe.pack(fill=BOTH, expand=YES)
+mycanvas = ResizingCanvas(myframe,width=850, height=400, bg="cyan", highlightthickness=0)
+mycanvas.pack(fill=BOTH, expand=YES)
 
+# Create and Add widgets
+launch = Button(window, text = "Launch Bot", command=BP)
+launch.pack()
 
 window.mainloop()
 
