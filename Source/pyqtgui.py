6bbc708ff9d2c543250f3cb427ff2f82a6477750
PulvarizerCoop1 <pulvarizercoop1@gmail.com>
2017-03-03 14:47:05 -0800

    Gui update

--- Source/pyqtgui.py
+++ Source/pyqtgui.py
@@ -1,30 +1,25 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import QtGui
-"""
+
 class SmashGui(Frame):
 
 	def __init__(self, parent):
 		Frame.__init__(self, parent)
 		self.parent = parent
 		self.initualizeWindow()
-		self.initualizeMenu()
-		self.initualizeButtons()
-		self.initualizeText()
-		self.initualizePlayerSelection()
+		#self.initualizeMenu()
+		#self.initualizeButtons()
+		#self.initualizeText()
+		#self.initualizePlayerSelection()
+		#self.show()
 
-	def initualizeWindow(self)
+	def initalizeWindow(self):
 		self.parent.setWindowTitle("Super Smash Bros AI")
-		self.parent.resize(250, 150)
-		self.parent.move(300, 300)
-"""
-		
-app = QApplication(sys.argv)
+		self.parent.setGeometry(500, 500, 450, 250)
+		w.setWindowIcon(QtGui.QIcon("SmashIcon.ico"))
 
-w = QWidget()
-w.resize(250, 250)
-w.move(300, 300)
-w.show()
-w.setWindowIcon(QtGui.QIcon("SmashIcon.ico"))
+window = QtGui.QWidget()
+
+SmashGui(window)
 
 sys.exit(app.exec_())
