559c014ea8781ff4092e5933bf79c3bb5e270104
PulvarizerCoop1 <pulvarizercoop1@gmail.com>
2017-03-08 13:03:38 -0800

    Recommiting gui
    Last commit didn't commit properly evidently.

--- pyqtgui.py
+++ pyqtgui.py
@@ -1,35 +1,21 @@
 import sys
-import BigProject
-from PyQt4 import QtGui
+from PyQt5 import QtGui
 
 class SmashGui(QtGui.QMainWindow):
 
 	def __init__(self):
 		super(SmashGui, self).__init__()
-		self.setGeometry(500, 500, 450, 250)
-		self.setWindowTitle("Super Smash Bros AI")
-		self.setWindowIcon(QtGui.QIcon('SmashIcon.ico'))
-		self.show()
+		self.initualizeWindow()
 		#self.initualizeMenu()
-		self.initualizeButtons()
+		#self.initualizeButtons()
 		#self.initualizeText()
 		#self.initualizePlayerSelection()
+		self.show()
 
-	def initualizeButtons(self):
-		# Create buttons
-		# btn.move(x,y)
-		save = QtGui.QPushButton("Save", self)
-		load = QtGui.QPushButton("Load", self)
-		launch = Button("Launch Bot", self)
-		launch.clicked.connect(self.BP())
-
-	# Launch BigProject.py
-	def BP():
-		BigProject
-
-
-	def onExit(self):
-		self.quit()
+	def initalizeWindow(self):
+		self.parent.setWindowTitle("Super Smash Bros AI")
+		self.parent.setGeometry(500, 500, 450, 250)
+		w.setWindowIcon(QtGui.QIcon("SmashIcon.ico"))
 
 app = QtGui.QApplication(sys.argv)
 
