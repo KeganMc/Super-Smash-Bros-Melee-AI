f4d7f03fa333196d5e3f5fe72fa955bcb8b8d2c9
PulvarizerCoop1 <pulvarizercoop1@gmail.com>
2017-03-08 13:24:21 -0800

    gui update

--- Source/pyqtgui.py
+++ Source/pyqtgui.py
@@ -1,25 +1,39 @@
 import sys
 from PyQt5 import QtGui
 
-class SmashGui(Frame):
+class SmashGui(QtGui.QMainWindow):
 
-	def __init__(self, parent):
-		Frame.__init__(self, parent)
-		self.parent = parent
+	def __init__(self):
+		super(SmashGui, self).__init__()
 		self.initualizeWindow()
 		#self.initualizeMenu()
 		#self.initualizeButtons()
 		#self.initualizeText()
 		#self.initualizePlayerSelection()
-		#self.show()
+		self.show()
 
 	def initalizeWindow(self):
 		self.parent.setWindowTitle("Super Smash Bros AI")
 		self.parent.setGeometry(500, 500, 450, 250)
 		w.setWindowIcon(QtGui.QIcon("SmashIcon.ico"))
 
-window = QtGui.QWidget()
+	def initializeButtons(self):
+"""		save = Button(self.parent, text = "Save Bot").grid(row=5, column=1)
+		load = Button(self.parent, text = "Load Bot").grid(row=5, column=2)
+		launch = Button(self.parent, text = "Launch Bot", command=self.BP).grid(row=5, column=3)"""
+		save = QtGui.QPushButton("Save", self)
+		#save.clicked.connect(#save function here)
+		load = QtGui.QPushButton("Load", self)
+		#load.clicked.connect("Load function here)
+		launch = QtGui.QPushButton("Launch", self)
+		launch.clicked.connect(self.BP)
 
-SmashGui(window)
+	# Launch BigProject.py
+	def BP(self):
+		BigProject
+
+app = QtGui.QApplication(sys.argv)
+
+GUI = SmashGui()
 
 sys.exit(app.exec_())
