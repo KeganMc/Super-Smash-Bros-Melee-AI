1c750f13218c35b32395dd8de5b9d4dabcc15aa9
PulvarizerCoop1 <pulvarizercoop1@gmail.com>
2017-03-08 13:39:47 -0800

    Gui Update

--- pyqtgui.py
+++ pyqtgui.py
@@ -17,6 +17,25 @@ class SmashGui(QtGui.QMainWindow):
 		self.parent.setGeometry(500, 500, 450, 250)
 		w.setWindowIcon(QtGui.QIcon("SmashIcon.ico"))
 
+	def initializeButtons(self):
+		save = QtGui.QPushButton("Save", self)
+		#save.clicked.connect(#save function here)
+		load = QtGui.QPushButton("Load", self)
+		#load.clicked.connect("Load function here)
+		launch = QtGui.QPushButton("Launch", self)
+		launch.clicked.connect(self.BP)
+
+	def initializeMenu(self):
+		fileAction = QtGui.QAction("New Game")
+		mainMenu = self.menuBar()
+		fileMenu = mainMenu.addMenu('&File')
+		fileMenu.addAction(fileAction)
+		aboutMenu = mainMenu.addMenu('&About')
+
+	# Launch BigProject.py
+	def BP(self):
+		BigProject
+
 app = QtGui.QApplication(sys.argv)
 
 GUI = SmashGui()
